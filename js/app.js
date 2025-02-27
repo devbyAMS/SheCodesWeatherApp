53f6e6bbe04fad8dc791c732447ecee444ecca6c
AnnaMariaSch <anna-maria-schmitt@gmx.de>
2025-02-27 06:34:47 +0100

    Inject Forecast HTML from JS

--- js/app.js
+++ js/app.js
@@ -52,7 +52,28 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecast = document.querySelector("#forecast");
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+ <div class="weatherforecastday">
+            <div class="weatherday">Mon</div>
+            <img src="icons/sunny.png" alt="Sunny" class="weather-app-icon" />
+            <div class="weatherforecasttemperatures">
+              <div class="degrees"><strong>15°</strong></div>
+              <div class="degrees">9°</div>
+            </div> `;
+  });
+}
+
+forecastElement.innerHTML = forecastHtml;
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
